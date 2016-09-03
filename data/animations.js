765434a5824c5e03f5438238080c69318e4d4af6
Mario Sergio Fuentes <msf1013@gmail.com>
2016-09-03 13:50:14 -0500

    First commit

---
+++ data/animations.js
@@ -0,0 +1,166 @@
+var arrRawData = [
+    {
+        "arrElements":[
+            {
+                "selector": "#id_1",
+                "markup": "<p id='id_1'>Parrafo 1</p>"
+            },
+            {
+                "selector": "#id_2",
+                "markup": "<h1 id='id_2'>Header 2</h1>"
+            },
+            {
+                "selector": "#id_3",
+                "markup": "<p id='id_3'><i>Italica 4</i></p>"
+            },
+            {
+                "selector": "#id_4",
+                "markup": "<div id='id_4'><select><option>O 1</option><option>O 2</option> <option>O 3</option> <option>O 4</option></select></div>"
+            }
+        ],
+        "arrAnimationSteps":[
+            {
+                "time": "500",
+                "arrEffectsIn" : ["animated fadeInDown"],
+                "arrEffectsOut": ["animated fadeOutUp"],
+                "arrSelectors" : ["#id_1"]
+            },
+            {
+                "time": "1000",
+                "arrEffectsIn" : ["animated bounceInLeft", "animated bounceInLeft"],
+                "arrEffectsOut": ["animated bounceOutRight", "animated bounceOutRight"],
+                "arrSelectors" : ["#id_2", "#id_3"]
+            },
+            {
+                "time": "1000",
+                "arrEffectsIn" : ["animated slideInRight"],
+                "arrEffectsOut": ["animated slideOutLeft"],
+                "arrSelectors" : ["#id_4"]
+            }
+        ],
+        "duration" : "2500"
+    },
+    {
+        "arrElements":[
+            {
+                "selector": "#id_1",
+                "markup": "<p id='id_1'>Parrafo 1</p>"
+            },
+            {
+                "selector": "#id_2",
+                "markup": "<h1 id='id_2'>Header 2</h1>"
+            },
+            {
+                "selector": "#id_3",
+                "markup": "<p id='id_3'><i>Italica 4</i></p>"
+            },
+            {
+                "selector": "#id_4",
+                "markup": "<div id='id_4'><select><option>O 1</option><option>O 2</option> <option>O 3</option> <option>O 4</option></select></div>"
+            }
+        ],
+        "arrAnimationSteps":[
+            {
+                "time": "500",
+                "arrEffectsIn" : ["animated fadeInDown"],
+                "arrEffectsOut": ["animated fadeOutUp"],
+                "arrSelectors" : ["#id_1"]
+            },
+            {
+                "time": "1500",
+                "arrEffectsIn" : ["animated bounceInLeft", "animated bounceInLeft"],
+                "arrEffectsOut": ["animated bounceOutRight", "animated bounceOutRight"],
+                "arrSelectors" : ["#id_2", "#id_3"]
+            },
+            {
+                "time": "1500",
+                "arrEffectsIn" : ["animated slideInRight"],
+                "arrEffectsOut": ["animated slideOutLeft"],
+                "arrSelectors" : ["#id_4"]
+            }
+        ],
+        "duration" : "2500"
+    },
+    {
+        "arrElements":[
+            {
+                "selector": "#id_1",
+                "markup": "<p id='id_1'>Parrafo 1</p>"
+            },
+            {
+                "selector": "#id_2",
+                "markup": "<h1 id='id_2'>Header 2</h1>"
+            },
+            {
+                "selector": "#id_3",
+                "markup": "<p id='id_3'><i>Italica 4</i></p>"
+            },
+            {
+                "selector": "#id_4",
+                "markup": "<div id='id_4'><select><option>O 1</option><option>O 2</option> <option>O 3</option> <option>O 4</option></select></div>"
+            }
+        ],
+        "arrAnimationSteps":[
+            {
+                "time": "500",
+                "arrEffectsIn" : ["animated fadeInDown"],
+                "arrEffectsOut": ["animated fadeOutUp"],
+                "arrSelectors" : ["#id_1"]
+            },
+            {
+                "time": "1500",
+                "arrEffectsIn" : ["animated bounceInLeft", "animated bounceInLeft"],
+                "arrEffectsOut": ["animated bounceOutRight", "animated bounceOutRight"],
+                "arrSelectors" : ["#id_2", "#id_3"]
+            },
+            {
+                "time": "1500",
+                "arrEffectsIn" : ["animated slideInRight"],
+                "arrEffectsOut": ["animated slideOutLeft"],
+                "arrSelectors" : ["#id_4"]
+            }
+        ],
+        "duration" : "2500"
+    },
+    {
+        "arrElements":[
+            {
+                "selector": "#id_1",
+                "markup": "<p id='id_1'>Parrafo 1</p>"
+            },
+            {
+                "selector": "#id_2",
+                "markup": "<h1 id='id_2'>Header 2</h1>"
+            },
+            {
+                "selector": "#id_3",
+                "markup": "<p id='id_3'><i>Italica 4</i></p>"
+            },
+            {
+                "selector": "#id_4",
+                "markup": "<div id='id_4'><select><option>O 1</option><option>O 2</option> <option>O 3</option> <option>O 4</option></select></div>"
+            }
+        ],
+        "arrAnimationSteps":[
+            {
+                "time": "500",
+                "arrEffectsIn" : ["animated fadeInDown"],
+                "arrEffectsOut": ["animated fadeOutUp"],
+                "arrSelectors" : ["#id_1"]
+            },
+            {
+                "time": "1500",
+                "arrEffectsIn" : ["animated bounceInLeft", "animated bounceInLeft"],
+                "arrEffectsOut": ["animated bounceOutRight", "animated bounceOutRight"],
+                "arrSelectors" : ["#id_2", "#id_3"]
+            },
+            {
+                "time": "1500",
+                "arrEffectsIn" : ["animated slideInRight"],
+                "arrEffectsOut": ["animated slideOutLeft"],
+                "arrSelectors" : ["#id_4"]
+            }
+        ],
+        "duration" : "2500"
+    }
+]
